132f3bf28dfe15874515a53e9348ec7d518bca94
tuanphamdvf <104239862+tuanphamdvf@users.noreply.github.com>
2023-05-16 16:08:33 +0700

    [Init]

---
+++ dashboard_rabbit/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Dashboard Rabbit",
+    "summary": "The dashboard includes a revenue chart, a profit chart, an order count chart, a product-wise revenue chart",
+    'description': """The dashboard includes a revenue chart, a profit chart, an order count chart, a product-wise revenue chart, an employee-wise revenue chart, total revenue, cash revenue, bank transfer revenue, purchasing expenses, and visually complete customer statistics.
+    """,
+    "price": "0",
+    "currency": "EUR",
+    'license': 'GPL-3',
+    'author': "TTN SOFTWARE",
+    'website': "TTNSOFTWARE.STORE",
+    'category': 'App',
+    'version': '15.1.1',
+    'depends': ['base', 'mrp', 'sale', 'pos_sale'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/owl_templates/owl_customer.xml',
+        'views/views.xml',
+        'views/templates.xml',
+
+
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'assets': {
+        'web.assets_qweb': [
+            'dashboard_rabbit/static/src/xml/*',
+            'dashboard_rabbit/static/src/xml/owl/*',
+        ],
+        'web.assets_backend': [
+            'dashboard_rabbit/static/src/components/**/*',
+            'dashboard_rabbit/static/src/scss/**/*',
+            'dashboard_rabbit/static/lib/*',
+            'dashboard_rabbit/static/src/js/**/*',
+            'dashboard_rabbit/static/src/js/*',
+        ],
+
+    },
+
+    'images': ['static/img/main_screenshot.gif']
+}
---
+++ dashboard_rabbit/models/owl_customer.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+
+class OwlCustomer(models.Model):
+    _name = "owl.customer"
+    _description = "Bar code lệnh sản xuất"
+
+    id = fields.Char(string="Id", required=True)
